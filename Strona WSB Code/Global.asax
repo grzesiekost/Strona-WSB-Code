3f20cd88ca0e610a585eccf3cc4cb0f0d33de805
ArkadioG <frank.gdansk@gmail.com>
2015-12-17 15:30:41 +0100

    Initial commit

---
+++ Strona WSB Code/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Strona_WSB_Code.MvcApplication" Language="C#" %>
---
+++ Strona WSB Code/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Strona_WSB_Code.Startup))]
+namespace Strona_WSB_Code
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
